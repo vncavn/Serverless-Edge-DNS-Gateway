9d004ced0d944076affbb76b475310a87264962c
vncavn <thanhnamnguyen161@gmail.com>
2026-08-21 04:43:37 -0400

    Fix curl command for downloading blocklists

--- update_lists.sh
+++ update_lists.sh
@@ -31,7 +31,7 @@ extract_domains() {
 
 echo "Downloading and processing blocklists..."
 curl -fsSL --max-time 60 \
-https://raw.githubusercontent.com/bibicadotnet/blocklist_minimal/main/blocklists.txt \
+https://raw.githubusercontent.com/vncavn/blocklist_minimal/refs/heads/main/blocklists.txt \
 | extract_domains > "$BLOCK_TMP"
 
 echo "Downloading and processing allowlists..."
